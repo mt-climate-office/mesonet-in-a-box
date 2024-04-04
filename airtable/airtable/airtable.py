bb58ea77f543f10fe33cfbe3fa8dd5b071158eab
Colin Brust <colin.brust@gmail.com>
2024-04-04 15:53:09 -0600

    lint and format.

--- airtable/airtable/airtable.py
+++ airtable/airtable/airtable.py
@@ -28,45 +28,53 @@ def search_tables_for_match(tables: list[Table], search_val: str) -> Table:
     for table in tables:
         if table == search_val:
             return table.id
-    
-    raise ValueError(f"The specified table ({search_val}) could not be found in your schema file.")
+
+    raise ValueError(
+        f"The specified table ({search_val}) could not be found in your schema file."
+    )
 
 
-def load_airtable_schema(schema: Path=Path("../at_schema.json")) -> dict[str, Any]: 
+def load_airtable_schema(schema: Path = Path("../at_schema.json")) -> dict[str, Any]:
     schema = json.load(schema.open())
-    schema['tables'] = [Table(**x) for x in schema['tables']]
+    schema["tables"] = [Table(**x) for x in schema["tables"]]
     return schema
 
 
-def _get_records(url: str, headers: dict[str, str], params: dict[str, Any], cur_records: Optional[list[dict[str, Any]]]=None) -> tuple[list[dict[str, Any]], str]:
+def _get_records(
+    url: str,
+    headers: dict[str, str],
+    params: dict[str, Any],
+    cur_records: Optional[list[dict[str, Any]]] = None,
+) -> tuple[list[dict[str, Any]], str]:
     if cur_records is None:
         cur_records = []
 
-    response = httpx.get(
-        url=url,
-        headers=headers,
-        params=params
-    )
+    response = httpx.get(url=url, headers=headers, params=params)
 
     data = response.json()
-    cur_records += data['records']
+    cur_records += data["records"]
 
     return cur_records, data.get("offset", None)
 
 
-def get_airtable_records(schema: dict[str, Any], table: str, fields: Optional[list[str]]=None, formula: str=None):
-    
-    table = search_tables_for_match(schema['tables'], table)
+def get_airtable_records(
+    schema: dict[str, Any],
+    table: str,
+    fields: Optional[list[str]] = None,
+    formula: str = None,
+):
+
+    table = search_tables_for_match(schema["tables"], table)
     url = f"{schema['api_url']}{schema['base_id']}/{table}"
     headers = {"Authorization": f"Bearer {getenv('AIRTABLE_API_KEY')}"}
 
     params = {}
     if fields:
-        params['fields[]'] = fields
-    
+        params["fields[]"] = fields
+
     if formula:
-        params['filterByFormula'] = formula
-    
+        params["filterByFormula"] = formula
+
     data, offset = _get_records(url, headers, params, None)
     while offset:
         params = {"offset": offset}
@@ -76,7 +84,9 @@ def get_airtable_records(schema: dict[str, Any], table: str, fields: Optional[li
     for record in data:
         record["fields"]["record"] = record["id"]
         record_fields = record["fields"]
-        record_fields = {k: v[0] if isinstance(v, list) else v for k, v in record_fields.items()}       
+        record_fields = {
+            k: v[0] if isinstance(v, list) else v for k, v in record_fields.items()
+        }
         out.append(record["fields"])
-    
+
     return pl.DataFrame(out)
