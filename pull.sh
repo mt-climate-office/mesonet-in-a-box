a73cca9406e8db3274e239e77657c589066164ed
Colin Brust <colin.brust@gmail.com>
2024-10-03 09:27:29 -0600

    Update pull script.

--- pull.sh
+++ pull.sh
@@ -3,3 +3,4 @@
 git pull
 git submodule sync --recursive
 git submodule update --init --remote --recursive
+git submodule foreach 'git checkout main || echo "Failed to checkout main in $name"; git pull origin main'
